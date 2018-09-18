2bce8ba639bba59f3e4fa4b50d7ea8428d57e663
Jon Cockle <yongfook@gmail.com>
2018-09-18 13:38:01 +0800

    revert to non JS hide/show upload

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -16,10 +16,4 @@ $(document).on('turbolinks:load',function(event){
 	  $('#chars_left').text(length);
 	});
 
-	$('#reupload_link').on('click', function(event) {
-		event.preventDefault()
-		$('#reupload').hide()
-		$('#hiddenupload').show()
-	})
-
 })
