d32e15415293e7d13dc6b2139f528d74cb78561e
Jon Cockle <yongfook@gmail.com>
2018-09-23 12:34:19 +0800

    need more coffee

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,22 @@
 module ApplicationHelper
 
 	def amazon_setup_is_incomplete?
-		return true if !ENV['AWS_REGION'].present?
-		return true if !ENV['AWS_BUCKET'].present?
-		return true if !ENV['AWS_ACCESS_KEY_ID'].present?
-		return true if !ENV['AWS_SECRET_ACCESS_KEY'].present?
+		return true if !ENV['AWS_REGION']
+		return true if !ENV['AWS_BUCKET']
+		return true if !ENV['AWS_ACCESS_KEY_ID']
+		return true if !ENV['AWS_SECRET_ACCESS_KEY']
 	end
 
 	def stripe_setup_is_incomplete?
-		return true if !ENV['stripe_api_key'].present?
-		return true if !ENV['stripe_publishable_key'].present?
+		return true if !ENV['stripe_api_key']
+		return true if !ENV['stripe_publishable_key']
 	end
 
 	def email_setup_is_incomplete?
-		return true if !ENV['SMTP_host'].present? && Rails.env.production?
-		return true if !ENV['SMTP_port'].present? && Rails.env.production?
-		return true if !ENV['SMTP_username'].present? && Rails.env.production?
-		return true if !ENV['SMTP_password'].present? && Rails.env.production?
+		return true if !ENV['SMTP_host'] && Rails.env.production? == true
+		return true if !ENV['SMTP_port'] && Rails.env.production? == true
+		return true if !ENV['SMTP_username'] && Rails.env.production? == true
+		return true if !ENV['SMTP_password'] && Rails.env.production? == true
 	end
 
 end
