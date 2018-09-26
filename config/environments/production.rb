fce8ac9209cd962f97522ac666e8d58384fcd17f
Jon Cockle <yongfook@gmail.com>
2018-09-26 12:21:38 +0800

    use cdn from config yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  if ENV['cdn_host'].present?
+    config.action_controller.asset_host = ENV['cdn_host']
+  end
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
