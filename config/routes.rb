6ccc292feff094d1006fa0360f63c3a98752dea7
Jaryl Sim <jaryl.sim@me.com>
2018-10-01 14:45:09 +0800

    Start moving admin-specific code into its own namespace.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
+
   devise_for :admins, controllers: { registrations: "registrations", sessions: "sessions" }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :customers
-  resources :products
+
+  namespace :admin do
+    resources :customers, only: [:index, :show]
+  end
+
   resources :payments
+  resources :products
   resources :links
+
   get 'view/:id', to: 'products#shop_show', as: 'shop_product'
   get 'success/:id', to: 'payments#success', as: 'success'
   get 'error', to: 'payments#error', as: 'error'
   get 'admin', to: 'payments#dashboard', as: 'dashboard'
   get '/payments/:id/create_and_send_new_link', to: 'payments#create_and_send_new_link', as: 'payment_create_and_send_new_link'
+
   root 'products#shop'
+
 end
